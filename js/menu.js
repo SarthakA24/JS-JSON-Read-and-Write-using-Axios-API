1fa956993f8290df81f9630384a8811b1edd44a2
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-10-22 12:49:55 +0530

    added the function getData() to get the data using Axios API

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,18 @@
 //Write code to get menu data from the json-server using axios API
+var menuData = [];
 
+getData = () => {
+    axios.get("http://localhost:3000/menu").then( response => {
+        menuData = response.data;
+    }).catch((error) => {
+        console.log("Data Fetch Error - " + error);
+    });
+}
 
 //Write code to load menu data using window onload event: getPromise is the promise result obained from Axios call
-window.onload = () => getPromise.then((response) => {
+// window.onload = () => getPromise.then((response) => {
 
-});
+// });
 
 //Write code to filter the menu item from list
 const category = document.getElementById('category');
