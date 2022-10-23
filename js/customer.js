be5774c435ea9e4682d0d6a1e3cedbf45debbe35
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-10-23 12:40:48 +0530

    added functions to validate form input data

--- js/customer.js
+++ js/customer.js
@@ -1,6 +1,10 @@
 //Write  password validation code here 
-function setPasswordConfirmValidity() {
-    
+function setPasswordConfirmValidity(custPasword, custConfirmPassword) {
+    if (custPasword != custConfirmPassword) {
+        return "Passwords does not match!";
+    } else {
+        return "";
+    }
 }
 
 
@@ -22,34 +26,65 @@ function validateData() {
         "idError" : validateId(customerId),
         "nameError" : validateName(name),
         "passwordError" : validatePassword(custPasword),
-        "passwordConfirmError" : setPasswordConfirmValidity(custConfirmPassword),
+        "passwordConfirmError" : setPasswordConfirmValidity(custPasword, custConfirmPassword),
         "emailError" : validateEmail(email),
         "contactError" : validateContactNumber(contactNumber),
         "addressError" : validateAddress(address)
-
     };
 }
 
 function validateId(customerId) {
-
+    if (customerId === "" || customerId === null || customerId === undefined) {
+        return "Please input this field!";
+    } else {
+        return "";
+    }
+}
+function validateName(name) {
+    if (name === "" || name === null || name === undefined) {
+        return "Please input this field!";
+    } else {
+        return "";
+    }
 }
 
 function validatePassword(password) {
-
-}
-
-function validateName(name) {
-
+    var regex = "[^ ]{8,}";
+    if (password === "" || password === null || password === undefined) {
+        return "Please input this field!";
+    } else if (!password.match(regex)) {
+        return "Invalid Password! Please enter min 8 characters!"
+    } else {
+        return "";
+    }
 }
 
 function validateEmail(email) {
-
+    var regex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+    if (email === '' || email === undefined || email === null){
+        return "Email cannot be empty";
+    } else if(!email.match(regex)) {
+        return "Invalid Email";
+    } else {
+        return "";
+    }
 }
 
 function validateContactNumber(number) {
-
+    var regex = "(?<!\d)\d{10}(?!\d)";
+    if (number === "" || number === undefined || number === null) {
+        return "Number cannot be empty."
+    } else if (!number.match(regex)) {
+        return "Invalid Number";
+    } else {
+        return "";
+    }
 }
 
 function validateAddress(address) {
-
+    if (address === "" || address === undefined || address === null) {
+        return "Address cannot be blank";
+    } else {
+        return "";
+    }
 }
